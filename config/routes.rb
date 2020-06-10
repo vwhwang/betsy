25ffbf1726e6ae5978fc2ad402f51f5275c48e55
Vera <lizeth.veraro@outlook.com>
2020-06-09 17:48:10 -0700

    Routes for login and logout also restful routes for merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "merchants#create"
+  delete "/logout", to: "merchants#destroy", as: "logout"
+
+  resources :merchants
+
 end
