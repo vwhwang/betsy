9a8d6f35cadcbac4b32fc5d5ddb7b59d7b835f8b
Vera <lizeth.veraro@outlook.com>
2020-06-09 17:47:05 -0700

    build from  github authorization hash

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,15 @@
 class Merchant < ApplicationRecord
   has_many :products
+
+  def self.build_from_github(auth_hash)
+    merchant = Merchant.new
+    merchant.uid = auth_hash[:uid]
+    merchant.provider = "github"
+    merchant.username = auth_hash["info"]["name"]
+    merchant.email = auth_hash["info"]["email"]
+
+    # Note that the merchant has not been saved.
+    # We'll choose to do the saving outside of this method
+    return merchant
+  end
 end
