e3776048efef8a050253544498b8cf04f36b6a9b
Jessica Stone <jstone4815@gmail.com>
2020-06-09 20:57:11 -0700

    add product#new and product#create, and create product#new view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,25 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params) 
+    if @product.save 
+      flash[:success] = "#{@product.name} has been added to the product databse"
+      redirect_to product_path(@product.id)
+      return
+    else
+      flash.now[:error] = "Something happened. #{@product.name} was not added."
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def product_params
+    return params.require(:product).permit(:name, :price, :inventory, :category, :merchant_id)
+  end
+
 end
