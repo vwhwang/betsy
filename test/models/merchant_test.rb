7144ecdc6e69a4b1f596e6f271a24123398cdd88
Vera <lizeth.veraro@outlook.com>
2020-06-09 20:45:49 -0700

    Model tests for validations and relationships

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,56 @@
 require "test_helper"
 
 describe Merchant do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    it "requires a username" do
+      merchant = Merchant.new
+      expect(merchant.valid?).must_equal false
+      expect(merchant.errors.messages).must_include :username
+    end
+
+    it "requires a unique username" do
+      username = "test username"
+      email = "test@gmail.com"
+      merchant1 = Merchant.new(username: username, email: email)
+
+      # This must go through, so we use create!
+      merchant1.save!
+
+      merchant2 = Merchant.new(username: username)
+      result = merchant2.save
+      expect(result).must_equal false
+      expect(merchant2.errors.messages).must_include :username
+    end
+
+    it "requires an email" do
+      merchant = Merchant.new
+      expect(merchant.valid?).must_equal false
+      expect(merchant.errors.messages).must_include :email
+    end
+
+    it "requires a unique email" do
+      username = "test username"
+      email = "test@gmail.com"
+      merchant1 = Merchant.new(username: username, email: email)
+
+      # This must go through, so we use create!
+      merchant1.save!
+
+      merchant2 = Merchant.new(email: email)
+      result = merchant2.save
+      expect(result).must_equal false
+      expect(merchant2.errors.messages).must_include :email
+    end
+  end
+
+  describe "relationships" do
+    it "has a list of products" do
+      merchant1 = merchants(:merchant_2)
+      expect(merchant1).must_respond_to :products
+      merchant1.products.each do |product|
+        expect(products).must_be_kind_of product
+      end
+    end
+  end
+
 end
