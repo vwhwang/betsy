631cd5d23c333b855a1004d8c4f5618c972b61e0
Vera <lizeth.veraro@outlook.com>
2020-06-11 14:35:14 -0700

    order create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,26 @@ class OrdersController < ApplicationController
     if session[:merchant_id]
       @order_items = OrderItem.by_merchant(session[:merchant_id])
     else
-      @order_items = OrderItem.all
+      @order_items = OrderItem.by_order_id(session[:order_id])
+    end
+  end
+
+  def create
+    if session[:order_id]
+      @order = Order.new(order_params)
+    else
+      @order = Order.new
+      session[:order_id] = @order.id
+    end
+
+    if @order.save
+      # sets the session order id
+      session[:order_id] = @order.id
+      flash[:success] = "Successfully created order"
+      return redirect_to order_path(@order.id)
+    else
+      flash[:error] = "Could not create order"
+      return redirect_to root_path
     end
   end
 
@@ -11,12 +30,16 @@ class OrdersController < ApplicationController
     if session[:order_id].nil?
       # creates a new order if one doesn't exist
       flash[:error] = "You dont have any products to check out"
-      redirect_to products_path
+      redirect_to root_path
     else
       # Use current order if there is alrady one:
-      @order_id = session[:order_id]
-      @order_items = OrderItem.by_order_id(@order_id)
-      
+      order_id = session[:order_id]
+      @order_products = OrderItem.by_order_id(order_id)
     end
   end
+
+  def order_params
+    return params.require(:order).permit(:name, :email, :address, :credit_card, :credit_card_exp)
+  end
 end
+
