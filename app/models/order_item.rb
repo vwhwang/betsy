c8d559fc4f533fc9bc7cd551edf1b8df297486c6
Vera <lizeth.veraro@outlook.com>
2020-06-10 17:08:43 -0700

    methods to seach by merchant and by order id

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,13 +2,24 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  # Return true if there is an inventory of the product.
-  def available?(quantity)
-    inventory - quantity > 0 ? true : false
+  def self.by_merchant(id)
+    order_items = OrderItem.all
+    result = []
+    order_items.each do |item|
+      if Product.find_by(id: item.product_id).merchant_id == id
+        result << item
+      end
+    end
+    return result
   end
 
-  def decrease_inventory(quantity)
-    self.available_inventory -= quantity
-    self.save
+  def self.by_order_id(id)
+    order_items = OrderItem.all
+    
+    items_list = order_items.select do |item|
+      OrderItem.find_by(order_id: id)
+    end
+
+    return items_list
   end
 end
