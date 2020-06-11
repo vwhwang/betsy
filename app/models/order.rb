23011de5471dacce80b6e0a59b3fa9bb5c9bac0a
Stephanie <stephaniejmars@gmail.com>
2020-06-10 19:25:12 -0700

    added status to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,33 @@
 class Order < ApplicationRecord
   has_many :order_items
+
+  # create_table "orders", force: :cascade do |t|
+  #   t.string "name"
+  #   t.string "email"
+  #   t.string "address"
+  #   t.integer "credit_card"
+  #   t.date "credit_card_exp"
+  #   t.datetime "created_at", precision: 6, null: false
+  #   t.datetime "updated_at", precision: 6, null: false
+  # end
+
+
+  #   add state to order model 
+
+  # Purchasing an order makes the following changes:
+  #   Reduces the number of inventory for each product 
+  #   Changes the order state from "pending" to "paid"
+  
+  #   Clears the current cart
+
+
+
+
+
+
+
+
+
+  
+
 end
