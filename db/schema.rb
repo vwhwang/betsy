748e561227d12d6ec9a0eea5720a20c0af17e5ad
Jessica Stone <jstone4815@gmail.com>
2020-06-11 08:52:46 -0700

    add status to order

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2020_06_11_022207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "merchants", force: :cascade do |t|
     t.integer "uid"
     t.string "provider"
