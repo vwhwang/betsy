40c3c5883daac9a8c87e0aa072c65b500e828f2e
Jessica Stone <jstone4815@gmail.com>
2020-06-11 17:42:06 -0700

    remove comments and add clear_cart method

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -24,20 +24,5 @@ describe Order do
     end
 
   end 
-
-  # #   Reduces the number of inventory for each product 
-  #   # loop through order items
-      
-  #   self.order_items.each do |item|
-  #     # find product - 
-  #       product = Product.find_by(id: item.product_id)
-  #     # inventory -= quantity of the order item
-  #       product.inventory -= (item.quantity)
-  #       product.save
-  #     end
-
-  # #   Changes the order state from "pending" to "paid"
-  #     self.status = "paid"
-  #     self.save
       
 end
