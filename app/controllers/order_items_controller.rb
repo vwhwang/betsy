597634f76d6ac4532774acf5375e51b8cc7f4438
Vera <lizeth.veraro@outlook.com>
2020-06-14 19:22:52 -0700

    Refactoring and adding validation for adding item with no stock

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -15,55 +15,38 @@ class OrderItemsController < ApplicationController
       return head :not_found
     end
 
-    if session[:order_id].nil?
-      # creates a new order if one doesn't exist
-      @order = Order.create
-      @order_id = @order.id
-
-      session[:order_id] = @order_id
-    else
-      # Use current order if there is alrady one:
-      @order_id = session[:order_id]
-      current_order = Order.find(@order_id)
-
-      current_item = current_order.order_items.find_by(product_id: params[:product_id])
-      # checks if item is already in order, updates by 1 if so
-      if current_item
-        # params[:quantity].to_i to add up the quantity
-        if @product.inventory < params[:quantity].to_i
-          flash[:error] = "Could not add item to order (not enough in stock)"
-          redirect_to product_path(@product.id)
-        else
-          current_item[:quantity] += params[:quantity].to_i
-          @product.decrease_inventory(params[:quantity].to_i)
-          current_item.save
-          flash[:success] = "Successfully updated order item"
-          redirect_to order_path(@order_id)
-        end
-        return
-      end
-    end
-
     if @product.inventory < 1 || @product.inventory < params[:quantity].to_i
-      flash[:error] = "Could not add item to order (not enough in stock)"
+      flash[:error] = "Could not add item to order! We have no stock available"
       redirect_to product_path(@product.id)
       return
     end
 
-    order_item = OrderItem.new({
-      product_id: params[:product_id],
-      quantity: params[:quantity].to_i,
-      order_id: session[:order_id],
-    })
+    # Call private method to create a new session if the session is nil.
+    manage_session
 
-    if order_item.save
-      @product.decrease_inventory(params[:quantity].to_i)
-      flash[:success] = "Item added to order"
-      redirect_to order_path(@order_id)
-    else
-      flash[:error] = "Could not add item to order"
-      redirect_to order_path(@order_id)
+    # Use current order if there is alrady one:
+    @order_id = session[:order_id]
+    current_order = Order.find(@order_id)
+
+    current_item = current_order.order_items.find_by(product_id: params[:product_id])
+    # checks if item is already in order, updates by 1 if so
+    if current_item
+      # params[:quantity].to_i to add up the quantity
+      if (current_item[:quantity] + params[:quantity].to_i) > @product.inventory
+        flash[:error] = "You cannot add more items than are in stock."
+        redirect_to product_path(@product.id)
+      else
+        current_item[:quantity] += params[:quantity].to_i
+        # @product.decrease_inventory(params[:quantity].to_i)
+        current_item.save
+        flash[:success] = "Successfully updated order item"
+        redirect_to order_path(@order_id)
+      end
+      return
     end
+    # Call method to create a new item if there is not current item in the order. 
+    create_new_orderItem
+
   end
 
   def destroy
@@ -71,7 +54,6 @@ class OrderItemsController < ApplicationController
     if order_item
       if order_item.destroy
         product = Product.find_by(id: order_item.product_id)
-        product.inventory_back(order_item.quantity)
         flash[:success] = "#{order_item.product.name} successfully removed from your basket!"
       else
         flash.now[:error] = "A problem occurred. #{order_item.product.name} was not successfully removed from your basket."
@@ -81,10 +63,17 @@ class OrderItemsController < ApplicationController
   end
 
   def update
-   order_item = OrderItem.find_by(id: params[:id].to_i)
+    order_item = OrderItem.find_by(id: params[:id].to_i)
+    product = Product.find_by(id: order_item.product_id)
+
     if order_item.nil?
       head :not_found
       return
+    end
+
+    if order_item_params[:quantity].to_i > product.inventory
+      flash[:error] = "You cannot add more items than are in stock"
+      redirect_to order_path(session[:order_id])
     elsif order_item.update(order_item_params)
       redirect_to order_path(session[:order_id]) # go to the index so we can see the driver in the list
       return
@@ -95,6 +84,35 @@ class OrderItemsController < ApplicationController
   end
 
   private
+
+  # Manage session to create a new session order id if the session order_id is nil
+  def manage_session
+    if session[:order_id].nil?
+      # creates a new order if one doesn't exist
+      @order = Order.create
+      @order_id = @order.id
+
+      session[:order_id] = @order_id
+    end
+  end
+
+  # Method to create a new order Item.
+  def create_new_orderItem
+    order_item = OrderItem.new({
+      product_id: params[:product_id],
+      quantity: params[:quantity].to_i,
+      order_id: session[:order_id],
+    })
+
+    if order_item.save
+      # @product.decrease_inventory(params[:quantity].to_i)
+      flash[:success] = "Item added to order"
+      redirect_to order_path(@order_id)
+    else
+      flash[:error] = "Could not add item to order"
+      redirect_to order_path(@order_id)
+    end
+  end
 
   def order_item_params
     return params.require(:order_item).permit(:quantity, :order_id, :product_id)
