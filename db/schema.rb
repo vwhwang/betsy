bc2cfc2630c17ce3eb300c9add5ce956e32a7891
Jessica Stone <jstone4815@gmail.com>
2020-06-15 14:48:44 -0700

    add active column to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_022207) do
+ActiveRecord::Schema.define(version: 2020_06_15_214700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_06_11_022207) do
     t.bigint "merchant_id"
     t.string "image"
     t.string "artist"
+    t.boolean "active"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
   end
 
