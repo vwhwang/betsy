84ea9b69468b80ca45ddf0d8b88c7d35c64d9fe3
Vera <lizeth.veraro@outlook.com>
2020-06-15 00:46:51 -0700

    test for update and destroy

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -15,39 +15,7 @@ describe OrderItemsController do
       order_nil = item.order_id.nil?
 
       expect(item.product_id).must_equal product.id
-      expect(item.product_id).must_equal product.id
       expect(item.quantity).must_equal item.quantity
-    end
-
-    it "can create an Order Item with a new Order" do
-      product = products(:product_2)
-      # assert that new Order is created
-      expect { post product_order_items_path(product.id) }.must_differ "Order.count", 1
-
-      must_respond_with :redirect
-      expect(flash[:success]).must_equal "Item added to order"
-
-      item = OrderItem.find_by(product_id: product.id)
-      order_nil = item.order_id.nil?
-      expect(item.product_id).must_equal product.id
-    end
-
-    it "can create an Order Item in a current Order" do
-      product = products(:product_2)
-      expect { post orders_path }.must_differ "Order.count", 1
-
-      # ensure that Order Item is not creating a new Order
-      expect { post product_order_items_path(product.id) }.must_differ "Order.count", 0
-      order_id = session[:order_id]
-
-      current_order = Order.find_by(id: order_id)
-      current_item = current_order.order_items.find_by(product_id: product.id)
-      current_item.quantity = 5
-
-      must_respond_with :redirect
-
-      expect(current_item.quantity).must_equal 5
-      expect(current_item.order_id).must_equal session[:order_id]
     end
 
     it "gets an error when there is not enough stock" do
@@ -58,8 +26,84 @@ describe OrderItemsController do
       product.update(inventory: 0)
 
       expect { post product_order_items_path(product.id) }.must_differ "OrderItem.count", 0
-      expect(flash[:error]).must_equal "Could not add item to order (not enough in stock)"
+      expect(flash[:error]).must_equal "Could not add item to order! We have no stock available"
       must_respond_with :redirect
+    end
+  end
+
+  describe "update" do
+    before do
+      @product = products(:product_2)
+      @order = orders(:order_1)
+
+      @params = {
+        order_item: {
+          order_id: @order.id,
+          product_id: @product.id,
+          quantity: 1,
+        },
+      }
+      post order_items_path(@params)
+      @item = OrderItem.find_by(product_id: @product)
+    end
+
+    it "succeeds for valid data and an existin order Item ID" do
+      updates = { order_item: { quantity: 2 } }
+
+      expect {
+        patch order_item_path(@item), params: updates
+      }.wont_change "OrderItem.count"
+
+      updated_order_item = OrderItem.find_by(id: @item.id)
+
+      expect(updated_order_item.quantity).wont_equal 6
+      must_respond_with :redirect
+    end
+
+    it "renders bad_request for invalid data" do
+      updates = { order_item: { product_id: nil } }
+
+      expect {
+        patch order_item_path(@item), params: updates
+      }.wont_change "OrderItem.count"
+
+      updated_order_item = OrderItem.find_by(id: @item.id)
+
+      expect(flash[:error]).must_equal "Your order items was not updated "
+      must_respond_with :redirect
+    end
+
+    it "renders 404 not_found for order item id" do
+      product = products(:product_2)
+      bogus_id = product.id
+      product.destroy
+
+      patch order_item_path(bogus_id), params: { order_item: { quantity: 2 } }
+
+      must_respond_with :not_found
+    end
+  end
+
+  describe "destroy" do
+    it "succeeds for an  order Item ID" do
+      item = order_items(:order_item_1)
+
+      expect {
+        delete order_item_path(item.id)
+      }.must_change "OrderItem.count", -1
+
+      must_respond_with :redirect
+    end
+
+    it "renders 404 not_found and does not update the DB for a invalid order item ID" do
+      item = order_items(:order_item_1)
+      item.id = -1
+
+      expect {
+        delete order_item_path(item.id)
+      }.wont_change "OrderItem.count"
+
+      must_respond_with :not_found
     end
   end
 end
