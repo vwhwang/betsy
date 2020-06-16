59f40b393ccedfc172895a4cadfb78a14d532bae
vwhwang <vwhwang38@gmail.com>
2020-06-16 12:24:26 -0700

    added update test for order control

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 describe OrdersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "update" do
+    it "will update order " do 
+      order = orders(:order_1)
+      order_hash = {
+        order: {
+          name: "updated jessica"
+        }
+      } 
+      expect {patch order_path(order.id), params: order_hash}.must_differ "Order.count", 0 
+      expect(order.reload.name).must_equal "updated jessica"
+
+    end 
+
+
+    it "can not update orders with status paid" do 
+      paid_order = orders(:paid_order)
+      order_hash = {
+        order: {
+          name: "updated bob"
+        }
+      } 
+      
+      expect {patch order_path(paid_order.id), params: order_hash}.must_differ "Order.count", 0 
+        
+      must_redirect_to  orders_path
+      expect(paid_order.reload.name).must_equal "bob"
+    end  
+
+  end 
+
+  describe "status" do 
+    it "can get status page" do 
+      paid_order = orders(:paid_order)
+    
+      get status_path(paid_order.id)
+
+      must_respond_with :ok
+    end 
+  end 
 end
