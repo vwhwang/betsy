2b27980640b48d322ead34ef01bae9c422e54206
Jessica Stone <jstone4815@gmail.com>
2020-06-16 15:22:49 -0700

    add find_product and verify_merchant helper methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,17 @@
 class ProductsController < ApplicationController
 
+  before_action :verify_merchant, only: [:edit, :update, :retire, :destroy]
+  before_action :find_product, only: [:show, :edit, :update, :retire, :destroy]
+
   skip_before_action :require_login, only: [:root, :show, :index]
 
   def index 
     @products = Product.all
   end 
 
-  def show 
-    product_id = params[:id]
-    @product = Product.find_by(id: product_id)
+  def show
+  end
 
-    if @product.nil?
-      redirect_to products_path
-      return 
-    end 
-  end 
-  
   def new
     @product = Product.new
   end
@@ -36,44 +32,26 @@ class ProductsController < ApplicationController
   end
 
   def edit 
-    @product = Product.find_by(id: params[:id])
-
-    if @product.nil? 
-      head :not_found
-      return 
-    end 
   end 
 
   def update 
-    @product = Product.find_by(id: params[:id])
-
-    if @product.nil? 
-      head :not_found 
-      return 
-    elsif @product.update(product_params)
+    if @product.update(product_params)
       flash[:success] = "Successfully updated product #{@product.name}"
       redirect_to product_path(@product.id)
       return 
     else  
-      flash.now[:error] = "can not update"
+      flash.now[:error] = "Could not update product #{@product.name}. Please see error message"
       render :edit
       return 
     end 
   end 
 
   def retire
-    @product= Product.find(params[:id])
-
-    if @product.nil? 
-      head :not_found 
-      return 
-    end
-
-    if @product.merchant_id != session[:merchant_id]
-      flash.now[:error] = "You cannot retire a product that doesn't belong to you"
-      redirect_to root_path
-      return
-    end
+    # if @product.merchant_id != session[:merchant_id]
+    #   flash.now[:error] = "You cannot retire a product that doesn't belong to you"
+    #   redirect_to root_path
+    #   return
+    # end
 
     if @product.update(active: false)
       flash[:success] = "#{@product.name} has been retired."
@@ -86,18 +64,10 @@ class ProductsController < ApplicationController
   end
 
   def destroy 
-    # currently anyone can delete work 
-    @product = Product.find_by(id: params[:id])
-
-    if @product.nil?
-      head :not_found
-      return
-    else   
-      @product.destroy
-      flash[:success] = "Art deleted"
-      redirect_to products_path
-      return 
-    end 
+    @product.destroy
+    flash[:success] = "#{@product.name} has been deleted from the product database."
+    redirect_to current_merchant_path
+    return 
   end 
 
 
@@ -106,5 +76,25 @@ class ProductsController < ApplicationController
   def product_params
     return params.require(:product).permit(:name, :price, :inventory, :image, :merchant_id, :active, category_ids:[])
   end
-  
+
+  def find_product
+    @product = Product.find_by(id: params[:id])
+    if @product.nil? 
+      flash[:error] = "Product could not be found."
+      redirect_to products_path
+      return 
+    end
+    return @product
+  end
+
+  def verify_merchant
+    if find_product
+      if @product.merchant_id != session[:merchant_id]
+        flash[:error] = "You are not authorized to do this."
+        redirect_back(fallback_location: root_path)
+        return
+      end
+    end
+  end
+
 end
