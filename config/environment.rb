92559da5bdae1dfd3c1ea26ee12f07a7d8e5ffb3
Jessica Stone <jstone4815@gmail.com>
2020-06-17 11:55:59 -0700

    add code to remove field_with_errors divs on form validation

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# remove extra divs that appear if there are form validation errors (they break the layout)
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
