3b2aaaab36c41bcfd35085491a58c0f2d95e8faa
Vera <lizeth.veraro@outlook.com>
2020-06-17 00:11:12 -0700

    Total revenue, #orders and revenue by status

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :products
+  has_many :order_items, through: :products
+
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
 
@@ -14,4 +16,45 @@ class Merchant < ApplicationRecord
     # We'll choose to do the saving outside of this method
     return merchant
   end
+
+  def total_revenue
+    @total_revenue = 0
+    self.order_items.each do |item|
+      if item.order.status == "paid" || item.order.status == "complete"
+        @total_revenue += item.total_per_item
+      end
+    end
+    return @total_revenue
+  end
+
+  def num_orders(status)
+    total_orders = 0
+
+    self.order_items.each do |order_item|
+      if order_item.order.status == status
+        total_orders += 1
+      end
+    end
+
+    return total_orders
+  end
+
+  def orders_by_status(status)
+    order_by_status = self.order_items.select do |order_item|
+      order_item.order.status == status
+    end
+    return order_by_status
+  end
+
+  def total_by_status(status)
+
+    total_by_status = 0
+    self.order_items.each do |item|
+      if item.order.status == status
+        total_by_status += item.order.order_cost
+      end
+    end
+    return total_by_status
+  end
+
 end
