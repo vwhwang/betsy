acbbed9c98131b57fa4935ae3f8ca58305f95e6a
Vera <lizeth.veraro@outlook.com>
2020-06-17 16:31:28 -0700

    add cancel order tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,30 +1,29 @@
 require "test_helper"
 
 describe Order do
-
-  describe "order_purchase" do 
-    it "reduces the number of inventory for each product" do 
+  describe "order_purchase" do
+    it "reduces the number of inventory for each product" do
       order = orders(:order_1)
       order_item1 = order_items(:order_item_1)
       order_item2 = order_items(:order_item_4)
 
       product1_count = order_item1.product.inventory
-      product2_count = order_item2.product.inventory 
+      product2_count = order_item2.product.inventory
 
       order.order_purchase
-      
+
       expect(order_item1.product.reload.inventory).must_equal product1_count - order_item1.quantity
       expect(order_item1.product.reload.inventory).must_equal product2_count - order_item2.quantity
     end
 
-    it "changes the order state from pending to paid" do 
+    it "changes the order state from pending to paid" do
       orders(:order_1).order_purchase
 
       expect(orders(:order_1).status).must_equal "paid"
     end
-  end   
+  end
 
-  describe "order_cost" do 
+  describe "order_cost" do
     it "should return correct order cost" do
       order = orders(:order_1)
       order_item1 = order_items(:order_item_1)
@@ -38,15 +37,29 @@ describe Order do
   end
 
   describe "relationships" do
-    it "can have many order_items" do 
+    it "can have many order_items" do
       product = products(:product_1)
       order = orders(:order_1)
 
       count = order.order_items.count
 
-      order_item1 = OrderItem.create(product: product, quantity:1, order:order)
-      order_item2 = OrderItem.create(product: product, quantity:1, order:order)
+      order_item1 = OrderItem.create(product: product, quantity: 1, order: order)
+      order_item2 = OrderItem.create(product: product, quantity: 1, order: order)
       expect(order.reload.order_items.count).must_equal count + 2
-    end 
+    end
+  end
+
+  describe "cancel_order_inventory" do
+    it "should return correct status and decrease the inventoty of the product" do
+      order = orders(:paid_order)
+      order_item5 = order_items(:order_item_5)
+      product_1 = products(:product_1)
+      order_item5.product_id
+
+      order.cancel_order_inventory
+      
+      expect(order.status).must_equal "cancelled"
+
+    end
   end
 end
